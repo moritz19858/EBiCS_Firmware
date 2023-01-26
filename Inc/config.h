f0d03622354bc662e1d4b785dd041ca23a963c0a
moritz19858 <123470006+moritz19858@users.noreply.github.com>
2023-01-26 12:04:53 +0100

    Speedlimit 35

--- Inc/config.h
+++ Inc/config.h
@@ -44,7 +44,7 @@
 #define THROTTLE_MAX 2800
 #define WHEEL_CIRCUMFERENCE 2200
 #define GEAR_RATIO 11 //dummy for testing
-#define SPEEDLIMIT 25
+#define SPEEDLIMIT 35
 #define PULSES_PER_REVOLUTION 1
 #define PH_CURRENT_MAX 600
 #define BATTERYCURRENT_MAX 10000
@@ -70,7 +70,7 @@
 #define P_FACTOR_PLL 10
 #define I_FACTOR_PLL 10
 #define AUTODETECT 1
-#define SPEED_PLL 0 //1 for using PLL, 0 for angle extrapolation
+//#define SPEED_PLL 0 //1 for using PLL, 0 for angle extrapolation
 
 #define EXTERNAL 1
 #define INTERNAL 0
